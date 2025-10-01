77e9e8d9cd2d9b5c31188eca7123745cb08ff0b4
Ivan Rozhchenko <rozhchenkoivan@gmail.com>
2025-10-01 21:12:58 +0300

    add time scale in TimeRange

--- src/Pages/Calendar/Schedule/Timeline/TimeRange.jsx
+++ src/Pages/Calendar/Schedule/Timeline/TimeRange.jsx
@@ -1,10 +1,17 @@
 function TimeRange({ start, end, time }) {
   return (
     <div
-      className={`p-1 row-start-${start} row-end-${end} content-start
+      className={`relative px-3 row-start-${start} row-end-${end} content-start
         border-t border-[#dde1e9] dark:border-[#6d6f72]`}
     >
       <p className="text-[14px] text-end font-extralight">{time}</p>
+      <div className="absolute top-0 right-0 h-full flex flex-col justify-between items-end">
+        <div className="w-3 border-t border-[#dde1e9] dark:border-[#6d6f72]"></div>
+        <div className="w-2 border-t border-[#dde1e9] dark:border-[#6d6f72]"></div>
+        <div className="w-3 border-t border-[#dde1e9] dark:border-[#6d6f72]"></div>
+        <div className="w-2 border-t border-[#dde1e9] dark:border-[#6d6f72]"></div>
+        <div className="w-3 border-t border-[#dde1e9] dark:border-[#6d6f72]"></div>
+      </div>
     </div>
   );
 }
