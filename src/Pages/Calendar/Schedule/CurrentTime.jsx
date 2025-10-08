cc9677af9d1bb060690815843f1e47868b227d54
Ivan Rozhchenko <rozhchenkoivan@gmail.com>
2025-10-08 14:06:05 +0300

    change lessons colors. place CurrentTime line over lessons

--- src/Pages/Calendar/Schedule/CurrentTime.jsx
+++ src/Pages/Calendar/Schedule/CurrentTime.jsx
@@ -32,7 +32,7 @@ function CurrentTime() {
     currentTimeInSeconds >= startTimeInSeconds &&
     currentTimeInSeconds <= endTimeInSeconds ? (
       <div
-        className="absolute w-full border-t border-red-500"
+        className="absolute z-2 w-full border-t border-red-500"
         style={{ top: `${getPosition()}px` }}
       ></div>
     ) : null
